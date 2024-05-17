51a822154a67f21762b2ab34d243968dccec637b
Braedydillon <161336297+Braedydillon@users.noreply.github.com>
2024-05-17 14:15:50 +0200

    Add files via upload

---
+++ src/test/java/com/mycompany/automated_unittesting/Unit_TestingTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.automated_unittesting;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author braed
+ */
+public class Unit_TestingTest {
+    
+    public Unit_TestingTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
